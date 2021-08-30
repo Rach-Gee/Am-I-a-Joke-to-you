e1fff4eac1019e23debf6ac3fd1ba5f935983db0
Luke Thompson <16175583curtin@gmail.com>
2021-08-30 20:49:26 +0800

    add models index

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./User');
+const Category = require('./Category');
+const Joke = require('./Joke');
+const Review = require('./Review');
+
+User.hasMany(Review, {
+  foreignKey: 'user_id'
+  });
+
+Review.belongsTo(User, {
+  foreignKey: 'user_id'
+  });
+
+Category.hasMany(Joke, {
+  foreignKey: 'category_id'
+  });
+
+Joke.belongsTo(Category, {
+  foreignKey: 'category_id'
+  });
+
+Joke.hasMany(Review, {
+  foreignKey: 'joke_id'
+  });
+
+Review.belongsTo(Joke, {
+  foreignKey: 'joke_id'
+  });
+
+module.exports = { User, Category, Joke, Review };
