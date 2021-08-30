1a5e60f7ea8e48e680dbf24904a0a4dcd5ddf232
Luke Thompson <16175583curtin@gmail.com>
2021-08-30 20:40:50 +0800

    add base models

---
+++ models/Review.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    rating: {
+      type: DataTypes.INT,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    joke_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'joke',
+        key: 'id',
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+  }
+);
+
+module.exports = Review;
