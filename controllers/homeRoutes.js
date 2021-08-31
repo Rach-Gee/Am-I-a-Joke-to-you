c6acbb85ea9f3779c749a51cb826df2837657be5
Luke Thompson <16175583curtin@gmail.com>
2021-08-31 18:54:26 +0800

    separate login information from homeroutes to avoid conflicts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -69,23 +69,4 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
-});
-
-router.get('/register', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-  res.render('register');
-});
-
-
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const login = require('./login');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/login', login);
 
 module.exports = router;
