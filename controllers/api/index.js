bc62ecfb7bacf384f4cd0e9df1c893a2631de816
Luke Thompson <16175583curtin@gmail.com>
2021-09-02 19:25:33 +0800

    add individual category routes, add ability to create jokes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const categoryRoutes = require('./categoryRoutes');
+const jokeRoutes = require('./jokeRoutes');
 
 router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
+router.use('/jokes', jokeRoutes);
 
 module.exports = router;
