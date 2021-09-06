20c412edc2893060a2b9976c847ad6f06b16f0cc
Rach Gee <Rach-Gee@hotmail.com>
2021-09-06 20:19:16 +0800

    working eslint

--- models/index.js
+++ models/index.js
@@ -5,26 +5,26 @@ const Review = require('./Review');
 
 User.hasMany(Review, {
   foreignKey: 'user_id'
-  });
+});
 
 Review.belongsTo(User, {
   foreignKey: 'user_id'
-  });
+});
 
 Category.hasMany(Joke, {
   foreignKey: 'category_id'
-  });
+});
 
 Joke.belongsTo(Category, {
   foreignKey: 'category_id'
-  });
+});
 
 Joke.hasMany(Review, {
   foreignKey: 'joke_id'
-  });
+});
 
 Review.belongsTo(Joke, {
   foreignKey: 'joke_id'
-  });
+});
 
 module.exports = { User, Category, Joke, Review };
