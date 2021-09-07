8a0da1bd727c83b96b71010c275adba6b06671f2
Luke Thompson <16175583curtin@gmail.com>
2021-09-07 19:12:34 +0800

    remove success alerts

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,17 @@
 
 $(document).ready(function () {
-    var options = {
-        max_value: 5,
-        step_size: 0.5,
-        selected_symbol_type: 'utf8_star',
-        initial_value: 3,
-        update_input_field_name: $("#input2"),
-    }
-    $(".rate").rate(options);
+  var options = {
+    max_value: 5,
+    step_size: 0.5,
+    selected_symbol_type: 'utf8_star',
+    initial_value: 3,
+    update_input_field_name: $('#input2'),
+  };
+  $('.rate').rate(options);
 
-    $(".rate").rate("setFace", 5, '😊');
-    $(".rate").rate("setFace", 1, '😒');
-})
+  $('.rate').rate('setFace', 5, '😊');
+  $('.rate').rate('setFace', 1, '😒');
+});
 
 
 
--- public/js/jokes.js
+++ public/js/jokes.js
@@ -1,32 +1,28 @@
 const newJokeHandler = async (event) => {
-    event.preventDefault();
-  
-    const title = document.querySelector('#joke-title').value.trim();
-    const content = document.querySelector('#joke-content').value.trim();
-    const category_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+  event.preventDefault();
+
+  const title = document.querySelector('#joke-title').value.trim();
+  const content = document.querySelector('#joke-content').value.trim();
+  const category_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
 
 
-    if (title && content) {
-      const response = await fetch(`/api/jokes`, {
-        method: 'POST',
-        body: JSON.stringify({ title, content, category_id, }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }); 
-      if (response.ok) {
-        alert('Joke Created!')
-        document.location.replace(`${category_id}`);
-      }       
-      else if (!response.ok) {
-        alert('You need to be logged in to do that!');
-      } else {
-        alert('Failed to create category');
-      }
+  if (title && content) {
+    const response = await fetch('/api/jokes', {
+      method: 'POST',
+      body: JSON.stringify({ title, content, category_id, }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.replace(`${category_id}`);
+    } else {
+      alert('Failed to create category');
     }
-  };
-  
-  document
-    .addEventListener('submit', newJokeHandler);
+  }
+};
+
+document
+  .addEventListener('submit', newJokeHandler);
